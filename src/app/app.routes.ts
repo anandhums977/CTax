dbd750aaf124b876b262d2087d320bc5c88760c7
appsteam.deepak <deepak.jose@appsteamtechnologies.com>
2024-09-04 20:01:30 +0530

    operations-manager-dahsboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/authentication/login/login.component';
-import { RootAdminDashboardComponent } from './features/roles/root-admin-dashboard/root-admin-dashboard.component';
+// import { RootAdminDashboardComponent } from './features/roles/root-admin-dashboard/root-admin-dashboard.component';
+import { DashboardComponent } from './features/roles/operations-manager/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect to login if no route is specified
   { path: 'login', component: LoginComponent },
-  { path: 'root-admin', component: RootAdminDashboardComponent },
+  // { path: 'root-admin', component: RootAdminDashboardComponent },
+  { path: 'operations-manager-dahsboard', component: DashboardComponent },
 ];
