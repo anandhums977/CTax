4bb810abd2ec94878946cf5acdeeb1f9452c6371
abinjoseph2050 <abinjoseph2050@gmail.com>
2024-09-04 13:59:48 +0530

    common components

---
+++ src/app/common/ribbon/ribbon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ribbon',
+  standalone: true,
+  imports: [],
+  templateUrl: './ribbon.component.html',
+  styleUrl: './ribbon.component.scss'
+})
+export class RibbonComponent {
+
+}
