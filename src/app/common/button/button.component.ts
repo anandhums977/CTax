79c9e10ad832451f05d8609dbf99357474408b1b
abinjoseph2050 <abinjoseph2050@gmail.com>
2024-09-04 14:23:12 +0530

    header UI

--- src/app/common/button/button.component.ts
+++ src/app/common/button/button.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+  styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
+  @Input() buttonLabel: string = '';
+  @Output() buttonclicked = new EventEmitter<any>();
 
+  onButtonClick(event: any) {
+    this.buttonclicked.emit(event);
+  }
 }
