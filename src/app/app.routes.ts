d04109a226aa0c8d7a04265b63cb6235c3b10f7c
abinjoseph2050 <abinjoseph2050@gmail.com>
2024-09-04 19:30:42 +0530

    root admin path update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/authentication/login/login.component';
-import { RootAdminComponent } from './roles/root-admin/root-admin.component';
+import { RootAdminComponent } from './features/root-admin/root-admin.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect to login if no route is specified
