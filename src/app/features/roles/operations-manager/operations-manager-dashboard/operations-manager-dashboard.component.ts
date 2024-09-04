9fcb5cf0525f24de605ab6c884449a92b53f5c56
appsteam.deepak <deepak.jose@appsteamtechnologies.com>
2024-09-04 20:03:15 +0530

    operations manager dahsboard added

---
+++ src/app/features/roles/operations-manager/operations-manager-dashboard/operations-manager-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operations-manager-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './operations-manager-dashboard.component.html',
+  styleUrl: './operations-manager-dashboard.component.scss'
+})
+export class OperationsManagerDashboardComponent {
+
+}
---
+++ src/app/features/roles/operations-manager/operations-managers-listing/operations-managers-listing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operations-managers-listing',
+  standalone: true,
+  imports: [],
+  templateUrl: './operations-managers-listing.component.html',
+  styleUrl: './operations-managers-listing.component.scss'
+})
+export class OperationsManagersListingComponent {
+
+}
