4dce971526394c91ce11943fcb8deea4fa995ce1
abinjoseph2050 <abinjoseph2050@gmail.com>
2024-09-05 21:42:25 +0530

    root admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/authentication/login/login.component';
-// import { RootAdminDashboardComponent } from './features/roles/root-admin-dashboard/root-admin-dashboard.component';
+import { RootAdminDashboardComponent } from './features/roles/root-admin-dashboard/root-admin-dashboard.component';
 import { OperationsManagerDashboardComponent } from './features/roles/operations-manager/operations-manager-dashboard/operations-manager-dashboard.component';
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect to login if no route is specified
   { path: 'login', component: LoginComponent },
-  // { path: 'root-admin', component: RootAdminDashboardComponent },
-  { path: 'operations-manager-dahsboard', component: OperationsManagerDashboardComponent },
+  { path: 'root-admin', component: RootAdminDashboardComponent },
+  {
+    path: 'operations-manager-dahsboard',
+    component: OperationsManagerDashboardComponent,
+  },
 ];
--- src/app/features/roles/root-admin-dashboard/root-admin-dashboard.component.ts
+++ src/app/features/roles/root-admin-dashboard/root-admin-dashboard.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { RibbonComponent } from '../../../common/ribbon/ribbon.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { SearchComponent } from '../../../common/search/search.component';
+import { ButtonComponent } from '../../../common/button/button.component';
 
 @Component({
   selector: 'app-root-admin-dashboard',
   standalone: true,
-  imports: [RibbonComponent],
+  imports: [
+    RibbonComponent,
+    MatTabsModule,
+    SearchComponent,
+    MatTableModule,
+    ButtonComponent,
+  ],
   templateUrl: './root-admin-dashboard.component.html',
   styleUrl: './root-admin-dashboard.component.scss',
 })
 export class RootAdminDashboardComponent {
+  name = 'John Doe';
+  location = 'Kochin';
   cardDetails = [
     {
       name: 'Admins',
@@ -23,4 +35,76 @@ export class RootAdminDashboardComponent {
       value: '23',
     },
   ];
+
+  tableData = [
+    {
+      id: 1,
+      name: 'Abin',
+      state: 'State 1',
+      username: 'abinusername',
+      password: 'Something@12546',
+      email: 'abin@hotmail.com',
+      actions: { accept: 'DELETE', reject: 'EDIT' },
+    },
+    {
+      id: 2,
+      name: 'Alex',
+      state: 'State 2',
+      username: 'alexusername',
+      password: 'Something@12546',
+      email: 'alex@hotmail.com',
+      actions: { accept: 'DELETE', reject: 'EDIT' },
+    },
+    {
+      id: 3,
+      name: 'Adam',
+      state: 'State 3',
+      username: 'adamusername',
+      password: 'Something@12546',
+      email: 'adam@hotmail.com',
+      actions: { accept: 'DELETE', reject: 'EDIT' },
+    },
+    {
+      id: 4,
+      name: 'Scott',
+      state: 'State 4',
+      username: 'scottusername',
+      password: 'Something@12546',
+      email: 'scott@hotmail.com',
+      actions: { accept: 'DELETE', reject: 'EDIT' },
+    },
+    {
+      id: 5,
+      name: 'Eddy',
+      state: 'State 5',
+      username: 'eddyusername',
+      password: 'Something@12546',
+      email: 'eddy@hotmail.com',
+      actions: { accept: 'DELETE', reject: 'EDIT' },
+    },
+    {
+      id: 6,
+      name: 'Max',
+      state: 'State 6',
+      username: 'maxusername',
+      password: 'Something@12546',
+      email: 'max@hotmail.com',
+      actions: { accept: 'DELETE', reject: 'EDIT' },
+    },
+  ];
+  displayedColumns = [
+    '#',
+    'name',
+    'state',
+    'username',
+    'password',
+    'email',
+    'actions',
+  ];
+
+  ngOnInit() {}
+
+  buttonClicked(event: any) {
+    console.log(event, 'from Root Admin Dash');
+  }
 }
