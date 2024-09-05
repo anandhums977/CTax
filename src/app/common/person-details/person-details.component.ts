7f0e0e1ec8eacdbf8533c1e70471640e9477cf9e
appsteam.deepak <deepak.jose@appsteamtechnologies.com>
2024-09-05 06:54:55 +0530

    table and pagitaor styles - added

---
+++ src/app/common/person-details/person-details.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'person-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './person-details.component.html',
+  styleUrl: './person-details.component.scss'
+})
+export class PersonDetailsComponent {
+  @Input() personDetails:any = {};
+}
--- src/app/features/roles/operations-manager/operations-manager-dashboard/operations-manager-dashboard.component.ts
+++ src/app/features/roles/operations-manager/operations-manager-dashboard/operations-manager-dashboard.component.ts
@@ -1,28 +1,124 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RibbonComponent } from '../../../../common/ribbon/ribbon.component';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 import { SearchComponent } from '../../../../common/search/search.component';
-import {MatTableModule} from '@angular/material/table';
-
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { PersonDetailsComponent } from '../../../../common/person-details/person-details.component';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-operations-manager-dashboard',
   standalone: true,
-  imports: [RibbonComponent,MatTabsModule,SearchComponent,MatTableModule],
+  imports: [
+    RibbonComponent,
+    MatPaginatorModule,
+    MatTabsModule,
+    SearchComponent,
+    MatTableModule,
+    PersonDetailsComponent,
+  ],
   templateUrl: './operations-manager-dashboard.component.html',
-  styleUrl: './operations-manager-dashboard.component.scss'
+  styleUrl: './operations-manager-dashboard.component.scss',
 })
 export class OperationsManagerDashboardComponent {
-  name:string = 'Deepak Jose';
-  location:string = 'Kochi'
-  tableData = [
-    { id: 1, name: "Alan", adminName: "Deepak", stand: "Stand 1", actions: { accept: "ACCEPT", reject: "REJECT" } },
-    { id: 2, name: "Don", adminName: "Abin", stand: "Stand 2", actions: { accept: "ACCEPT", reject: "REJECT" } },
-    { id: 3, name: "George", adminName: "Anandhu", stand: "Stand 3", actions: { accept: "ACCEPT", reject: "REJECT" } },
-    { id: 4, name: "Tojo", adminName: "Krishnana", stand: "Stand 4", actions: { accept: "ACCEPT", reject: "REJECT" } },
-    { id: 5, name: "Jerin", adminName: "Unni", stand: "Stand 5", actions: { accept: "ACCEPT", reject: "REJECT" } }
+  name: string = 'Deepak Jose';
+  location: string = 'Kochi';
+  tcaApprovalData =  new MatTableDataSource([
+    {
+      id: 1,
+      name: 'Alan',
+      adminName: 'Deepak',
+      stand: 'Stand 1',
+      actions: { accept: 'ACCEPT', reject: 'REJECT' },
+    },
+    {
+      id: 2,
+      name: 'Don',
+      adminName: 'Abin',
+      stand: 'Stand 2',
+      actions: { accept: 'ACCEPT', reject: 'REJECT' },
+    },
+    {
+      id: 3,
+      name: 'George',
+      adminName: 'Anandhu',
+      stand: 'Stand 3',
+      actions: { accept: 'ACCEPT', reject: 'REJECT' },
+    },
+    {
+      id: 4,
+      name: 'Tojo',
+      adminName: 'Krishnana',
+      stand: 'Stand 4',
+      actions: { accept: 'ACCEPT', reject: 'REJECT' },
+    },
+    {
+      id: 5,
+      name: 'Jerin',
+      adminName: 'Unni',
+      stand: 'Stand 5',
+      actions: { accept: 'ACCEPT', reject: 'REJECT' },
+    },
+  ]);
+  tcaActivationData =  new MatTableDataSource([
+    {
+      id: 1,
+      name: 'Alan',
+      adminName: 'Deepak',
+      stand: 'Stand 1',
+      pos: 'POS 1',
+      actions: { accept: 'ACCEPT', reject: 'REJECT' },
+    },
+    {
+      id: 2,
+      name: 'Don',
+      adminName: 'Abin',
+      stand: 'Stand 2',
+      pos: 'POS 2',
+      actions: { accept: 'ACCEPT', reject: 'REJECT' },
+    },
+    {
+      id: 3,
+      name: 'George',
+      adminName: 'Anandhu',
+      stand: 'Stand 3',
+      pos: 'POS 3',
+      actions: { accept: 'ACCEPT', reject: 'REJECT' },
+    },
+    {
+      id: 4,
+      name: 'Tojo',
+      adminName: 'Krishnana',
+      stand: 'Stand 4',
+      pos: 'POS 4',
+      actions: { accept: 'ACCEPT', reject: 'REJECT' },
+    },
+    {
+      id: 5,
+      name: 'Jerin',
+      adminName: 'Unni',
+      stand: 'Stand 5',
+      pos: 'POS 5',
+      actions: { accept: 'ACCEPT', reject: 'REJECT' },
+    },
+  ]);
+
+  displayedColumnsTcaApprovalData = [
+    '#',
+    'name',
+    'adminName',
+    'stand',
+    'actions',
   ];
-  displayedColumns=['#','name', 'adminName', 'stand', 'actions']
+  displayedColumnsTcaActivationData = [
+    '#',
+    'name',
+    'adminName',
+    'stand',
+    'pos',
+    'actions',
+  ];
+
   cardDetails = [
     {
       name: 'Agent Admins',
@@ -38,6 +134,12 @@ export class OperationsManagerDashboardComponent {
     },
   ];
 
-  ngOnInit(){
+  @ViewChild('paginator1') paginator1!: MatPaginator;
+  @ViewChild('paginator2') paginator2!: MatPaginator;
+
+
+  ngOnInit() {
+    this.tcaActivationData.paginator = this.paginator2
+    this.tcaApprovalData.paginator = this.paginator1
   }
 }
