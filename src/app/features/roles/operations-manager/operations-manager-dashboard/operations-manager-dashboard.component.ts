a13a12a090977551cffa3f5f3591820ab8543eee
appsteam.deepak <deepak.jose@appsteamtechnologies.com>
2024-09-07 05:03:34 +0530

    filter style

--- src/app/features/roles/operations-manager/operations-manager-dashboard/operations-manager-dashboard.component.ts
+++ src/app/features/roles/operations-manager/operations-manager-dashboard/operations-manager-dashboard.component.ts
@@ -5,16 +5,24 @@ import { SearchComponent } from '../../../../common/search/search.component';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import {Dialog, DialogModule} from '@angular/cdk/dialog';
+import { PersonDetailsComponent } from '../../../../common/person-details/person-details.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 @Component({
   selector: 'app-operations-manager-dashboard',
   standalone: true,
   imports: [
     RibbonComponent,
     MatPaginatorModule,
+    MatSelectModule,
+    MatFormFieldModule,
     MatTabsModule,
     SearchComponent,
     DialogModule,
     MatTableModule,
+    MatMenuModule
   ],
   templateUrl: './operations-manager-dashboard.component.html',
   styleUrl: './operations-manager-dashboard.component.scss',
@@ -23,6 +31,20 @@ export class OperationsManagerDashboardComponent {
   name: string = 'Deepak Jose';
   location: string = 'Kochi';
   dialog = inject(Dialog);
+  standsList =[
+    {
+    id:1,
+    name:'Stand 1'
+  },
+  {
+    id:2,
+    name:'Stand 2'
+  },
+  {
+    id:3,
+    name:'Stand 3'
+  }
+]
   tcaApprovalData =  new MatTableDataSource([
     {
       id: 1,
@@ -141,5 +163,11 @@ export class OperationsManagerDashboardComponent {
   ngOnInit() {
     this.tcaActivationData.paginator = this.paginator2
     this.tcaApprovalData.paginator = this.paginator1
+  }
+  openPersonDeatilsDialog(data:any) {
+    this.dialog.open(PersonDetailsComponent, {
+      minWidth: '300px',
+      data: data
+    });
   }
 }
