db0ac7f07a43901af4b9a5b964a6244a697adfc2
abinjoseph2050 <abinjoseph2050@gmail.com>
2024-09-08 21:54:01 +0530

    footer changes

--- src/app/theme/theme/theme.component.ts
+++ src/app/theme/theme/theme.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
 import { RouterOutlet } from '@angular/router';
-import { NewFooterComponent } from '../new-footer/new-footer.component';
+import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-theme',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterOutlet, NewFooterComponent],
+  imports: [HeaderComponent, RouterOutlet, FooterComponent],
   templateUrl: './theme.component.html',
   styleUrl: './theme.component.scss',
 })
